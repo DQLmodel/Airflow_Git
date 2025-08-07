b4ad778a79cce0cf2dfb4ea112cd1ea13886b521
praniade <adeprani@dqlabs.ai>
2025-08-07 11:48:19 +0530

    Update mssql_to_snowflake_example.py

--- airflow/dags/mssql_to_snowflake_example.py
+++ airflow/dags/mssql_to_snowflake_example.py
@@ -1,4 +1,4 @@
-from airflow import DAG
+from airflow import DA
 from airflow.operators.python import PythonOperator
 from datetime import date
 # from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
