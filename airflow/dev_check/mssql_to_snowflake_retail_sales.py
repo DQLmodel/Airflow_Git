8622b2d87398a423d309a9aee877f76f7e8683d3
praniade <adeprani@dqlabs.ai>
2025-09-18 15:51:36 +0530

    Update mssql_to_snowflake_retail_sales.py

--- airflow/dev_check/mssql_to_snowflake_retail_sales.py
+++ airflow/dev_check/mssql_to_snowflake_retail_sales.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.exceptions import AirflowFailException
 from datetime import datetime, timedelta
-import pandas as pprani
+import pandas as pp
 import tempfile
 import os
 import time
